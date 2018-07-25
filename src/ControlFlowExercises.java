c8c56b00ee10eddeb232a9eb14f54a42e7462939
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-07-25 08:42:57 -0500

    updated control statements and loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -87,58 +87,63 @@ public class ControlFlowExercises {
 //        }
 //
         // 4
-        System.out.print("Enter a grade number between 0 and 100: ");
-        int grade = sc.nextInt();
-        if (grade <= 100 && grade >= 97) {
-            System.out.println("A+");
-        } else if (grade <= 96 && grade >= 93) {
-            System.out.println("A");
-        } else if (grade <= 92 && grade >= 90) {
-            System.out.println("A-");
-        } else if (grade <= 89 && grade >= 87) {
-            System.out.println("B+");
-        } else if (grade <= 86 && grade >= 83) {
-            System.out.println("B");
-        } else if (grade <= 82 && grade >= 80) {
-            System.out.println("B-");
-        } else if (grade <= 79 && grade >= 77) {
-            System.out.println("C+");
-        } else if (grade <= 76 && grade >= 73) {
-            System.out.println("C");
-        } else if (grade <= 72 && grade >= 70) {
-            System.out.println("C-");
-        } else if (grade <= 69 && grade >= 67) {
-            System.out.println("D+");
-        } else if (grade <= 66 && grade >= 63) {
-            System.out.println("D");
-        } else if (grade <= 62 && grade >= 60) {
-            System.out.println("D-");
-        } else if (grade <= 59 && grade >= 0) {
-            System.out.println("F");
-        } else {
-            System.out.println("That was not a number between 0 and 100");
-        }
+        int j = 0;
+        for (int i = 0; i <= j;i += 1) {
+            System.out.print("Enter a grade number between 0 and 100: ");
+            int grade = sc.nextInt();
+            if (grade <= 100 && grade >= 97) {
+                System.out.println("A+");
+            } else if (grade <= 96 && grade >= 93) {
+                System.out.println("A");
+            } else if (grade <= 92 && grade >= 90) {
+                System.out.println("A-");
+            } else if (grade <= 89 && grade >= 87) {
+                System.out.println("B+");
+            } else if (grade <= 86 && grade >= 83) {
+                System.out.println("B");
+            } else if (grade <= 82 && grade >= 80) {
+                System.out.println("B-");
+            } else if (grade <= 79 && grade >= 77) {
+                System.out.println("C+");
+            } else if (grade <= 76 && grade >= 73) {
+                System.out.println("C");
+            } else if (grade <= 72 && grade >= 70) {
+                System.out.println("C-");
+            } else if (grade <= 69 && grade >= 67) {
+                System.out.println("D+");
+            } else if (grade <= 66 && grade >= 63) {
+                System.out.println("D");
+            } else if (grade <= 62 && grade >= 60) {
+                System.out.println("D-");
+            } else if (grade <= 59 && grade >= 0) {
+                System.out.println("F");
+            } else {
+                System.out.println("That was not a number between 0 and 100");
+            }
 
-        System.out.print("Continue? [y/N] ");
-        String userInput = sc.next();
-        boolean confirmation = userInput.equals("y");
+            System.out.print("Continue? [y/N] ");
+            String userInput = sc.next();
+            boolean confirmation = userInput.equals("y");
 
-        if (confirmation) {
-            System.out.println("A+: 100-97");
-            System.out.println("A :  96-93");
-            System.out.println("A-:  92-90");
-            System.out.println("B+:  89-97");
-            System.out.println("B :  86-83");
-            System.out.println("B-:  82-80");
-            System.out.println("C+:  70-77");
-            System.out.println("C :  76-73");
-            System.out.println("C-:  72-70");
-            System.out.println("D+:  60-67");
-            System.out.println("D :  66-63");
-            System.out.println("D-:  62-60");
-            System.out.println("F :  59- 0");
-        } else {
-            System.out.println("Grade list not shown");
+            if (confirmation) {
+                j += 1;
+            } else {
+                System.out.print(
+                        "A+: 100-97\n" +
+                        "A :  96-93\n" +
+                        "A-:  92-90\n" +
+                        "B+:  89-97\n" +
+                        "B :  86-83\n" +
+                        "B-:  82-80\n" +
+                        "C+:  70-77\n" +
+                        "C :  76-73\n" +
+                        "C-:  72-70\n" +
+                        "D+:  60-67\n" +
+                        "D :  66-63\n" +
+                        "D-:  62-60\n" +
+                        "F :  59- 0");
+            }
+
         }
 
     }
---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String message = "We don't need no education \nWe don't need no thought control";
+        System.out.printf("%s %n", message);
+
+        String message2 = "Check \"this\" out!, \"s inside of \"s";
+        System.out.printf("%s %n", message2);
+
+        String message3 = "In windows, the main drive is usually C:\\";
+        System.out.printf("%s %n", message3);
+
+        String message4 = "I can do backslashes \\, double backslashes \\\\, \nand the amazing tripple backslash \\\\\\";
+        System.out.printf("%s %n", message4);
+
+    }
+
+}
